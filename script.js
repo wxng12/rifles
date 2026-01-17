e07dfbf339c03c09df03e84c979e37402972603d
wxng12 <wingsw.test@gmail.com>
2026-01-16 18:39:10 -0800

    update
    e

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-# a
+function promo() {
+    if (document.getElementById('promo').style.display != 'none') {
+        document.getElementById('promo').style.display = 'none';
+    }
+    else {
+        document.getElementById('promo').style.display = 'block';
+    }
+}
+
+function battle() {
+    if (document.getElementById('battle').style.display != 'none') {
+        document.getElementById('battle').style.display = 'none';
+    }
+    else {
+        document.getElementById('battle').style.display = 'block';
+    }
+}
+
+function uni() {
+    if (document.getElementById('uni').style.display != 'none') {
+        document.getElementById('uni').style.display = 'none';
+    }
+    else {
+        document.getElementById('uni').style.display = 'block';
+    }
+}
+
+function staff() {
+    if (document.getElementById('staff').style.display != 'none') {
+        document.getElementById('staff').style.display = 'none';
+    }
+    else {
+        document.getElementById('staff').style.display = 'block';
+    }
+}
